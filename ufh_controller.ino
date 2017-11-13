35d2c863685f607b862fc6fe1b924301d58f090a
Tom <tom@augmentautomotive.co.uk>
2017-11-13 19:01:29 +0000

    Test 1hz intrerrupt timer

--- ufh_controller.ino
+++ ufh_controller.ino
@@ -21,17 +21,37 @@ FilterOnePole subFloorTempFilter( LOWPASS, 0.001);
 FilterOnePole middleFloorTempFilter( LOWPASS, 0.001);
 FilterOnePole fiveVoltRail( LOWPASS, 0.001);
 
+boolean toggle1 = 0;
+
 // the setup function runs once when you press reset or power the board
 void setup() 
 {
   // initialize digital pin 13 as an output.
   pinMode(13, OUTPUT);
   
+  pinMode(LED_BUILTIN, OUTPUT);
+  
   Serial.begin(9600);
 
   subFloorTempFilter.setToNewValue(getTemp(0));
   middleFloorTempFilter.setToNewValue(getTemp(1));
   fiveVoltRail.setToNewValue(analogRead(2));
+  
+  cli();
+  
+  TCCR1A = 0;// set entire TCCR1A register to 0
+  TCCR1B = 0;// same for TCCR1B
+  TCNT1  = 0;//initialize counter value to 0
+  // set compare match register for 1hz increments
+  OCR1A = 15624;// = (16*10^6) / (1*1024) - 1 (must be <65536)
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS12 and CS10 bits for 1024 prescaler
+  TCCR1B |= (1 << CS12) | (1 << CS10);  
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+  
+  sei();
 }  
 
 int ufhActive = 0;
@@ -95,6 +115,18 @@ void loop() {
   }
 }
 
+ISR(TIMER1_COMPA_vect){//timer1 interrupt 1Hz toggles pin 13 (LED)
+  //generates pulse wave of frequency 1Hz/2 = 0.5kHz (takes two cycles for full wave- toggle high then toggle low)
+  if (toggle1){
+    digitalWrite(LED_BUILTIN,HIGH);
+    toggle1 = 0;
+  }
+  else{
+    digitalWrite(LED_BUILTIN,LOW);
+    toggle1 = 1;
+  }
+}
+
 //--------------------------
 double getTemp(int analogueChannel) {
   // Inputs ADC Value from Thermistor and outputs Temperature in Celsius
@@ -126,3 +158,5 @@ double getTemp(int analogueChannel) {
 
   return Temp;  // Return the Temperature
 }
+
+
